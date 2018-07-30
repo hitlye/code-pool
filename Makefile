6e083ce6d73e144aa9f1e3d8c7f3e0e08afbaa61
Leo <hit.lye@gmail.com>
2018-07-30 15:57:24 -0700

    update two_sum solution

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 # Code Style Check
 
+check:
+	cpplint leetcode/*
+	cppcheck --enable=all --platform=unix64 --std=c11 --suppress=unusedFunction --suppress=missingInclude leetcode/
+
 install-c++:
 	pip install cpplint
 	brew install cppcheck
-
-check:
-	cpplint leetcode/*
-	cppcheck --enable=all --platform=unix64 --std=c11 leetcode/
--- leetcode/two_sum.cpp
+++ leetcode/two_sum.cpp
@@ -1,8 +1,63 @@
 // Copyright 2018 Leo - All Rights Reserved
 // Author: Leo
+// Best solution time: O(n)
+// Best solution space: O(n)
 
-#include <iostream>
+// Solution dependencies
+#include <vector>
+#include <unordered_map>
 
-int main() {
-  return 0;
-}
+using std::vector;
+using std::unordered_map;
+
+// Core: Use the hash map to store previously checked nums
+// Trick: map_key: target - num
+//        map_value: index of num
+//
+// Time: O(n)
+// Space: O(n)
+class Solution {
+ public:
+  vector<int> twoSum(const vector<int>& nums, int target) {
+    unordered_map<int, int> cache;
+
+    for (int i = 0; i < nums.size(); ++i) {
+      // hit target - nums[i] in cache
+      const auto got = cache.find(target - nums[i]);
+      if (got != cache.end()) {
+        return {got->second, i};
+      }
+      // not hit
+      cache.insert({nums[i], i});
+    }
+
+    return {-1, -1};
+  }
+};
+
+// Follow up 1. If input array is sorted and space complexity should be O(1)
+// Trick: two pointers from head and tail
+//
+//   1, 2, 3, 4, 5, 6, 7
+//   ^                 ^
+//   phead             ptail
+//
+//  phead + ptail < target => phead++
+//                > target => ptail--
+class Solution2 {
+ public:
+  vector<int> twoSum(const vector<int>& nums, int target) {
+    int phead = 0;
+    int ptail = nums.size() - 1;
+
+    while (phead < ptail) {
+      if (nums[phead] + nums[ptail] < target)
+        ++phead;
+      else if (nums[phead] + nums[ptail] > target)
+        --ptail;
+      else
+        return {phead, ptail};
+    }
+    return {-1, -1};
+  }
+};
