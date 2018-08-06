9e7ff93815dd219daacd6f364806f2ba135f5352
Haiyang Yu <dennanisny@hotmail.com>
2018-08-06 17:35:40 +0800

    [java] add IntelliJ Google Java Style config and update ZigZagConversion

--- leetcode/java/ZigZagConversion.java
+++ leetcode/java/ZigZagConversion.java
@@ -2,28 +2,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ZigZagConversion {
-    public String convert(String s, int numRows) {
-        if (numRows == 1 || s.isEmpty())
-            return s;
+  public String convert(String s, int numRows) {
+    if (numRows == 1 || s.isEmpty())
+      return s;
 
-        Map<Integer, StringBuilder> mapping = new HashMap<>();
-        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
-            mapping.put(i, new StringBuilder());
-        }
+    Map<Integer, StringBuilder> mapping = new HashMap<>();
+    for (int i = 0; i < Math.min(numRows, s.length()); i++)
+      mapping.put(i, new StringBuilder());
 
-        int row = 0;
-        boolean goDown = true;
-        for (char c : s.toCharArray()) {
-            mapping.get(row).append(c);
-            if ((goDown && row == numRows - 1) || (!goDown && row == 0))
-                goDown = !goDown;
-            row += goDown ? 1 : -1;
-        }
+    int row = 0;
+    boolean goDown = true;
 
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < Math.min(numRows, s.length()); i++)
-            output.append(mapping.get(i).toString());
-
-        return output.toString();
+    for (char c : s.toCharArray()) {
+      mapping.get(row).append(c);
+      if ((goDown && row == numRows - 1) || (!goDown && row == 0))
+        goDown = !goDown;
+      row += goDown ? 1 : -1;
     }
+
+    StringBuilder output = new StringBuilder();
+    for (int i = 0; i < Math.min(numRows, s.length()); i++)
+      output.append(mapping.get(i).toString());
+
+    return output.toString();
+  }
 }
