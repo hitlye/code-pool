3361501b1d7ef924dae36e271ffbaa0f45981fd1
Leo <hit.lye@gmail.com>
2018-09-16 21:04:06 -0700

    add intersection of two arrays and reverse vowels of a string
    solutions

---
+++ leetcode/cpp/intersection_of_two_arrays.cpp
@@ -0,0 +1,17 @@
+// Copyright 2018 Leo - All Rights Reserved
+// Author: Leo
+// Best solution time: O(n)
+// Best solution space: O(n)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+      set<int> nums(nums1.begin(), nums1.end());
+
+      set<int> intersection_nums;
+      for (auto &n : nums2) {
+        if (nums.find(n) != nums.end()) intersection_nums.insert(n);
+      }
+
+      return vector<int>(intersection_nums.begin(), intersection_nums.end());
+    }
+};
---
+++ leetcode/cpp/reverse_vowels_of_a_string.cpp
@@ -0,0 +1,23 @@
+// Copyright 2018 Leo - All Rights Reserved
+// Author: Leo
+// Best solution time: O(xx)
+// Best solution space: O(xx)
+
+// Two pointer keeps track of vowels
+class Solution {
+ public:
+  string reverseVowels(string s) {
+    set<char> vowels_table = {'a', 'e', 'i', 'o', 'u'};
+
+    int l = 0, r = s.size() - 1;
+    while (l < r) {
+      while (l < r && vowels_table.find(tolower(s[l])) == vowels_table.end()) ++l;
+      while (l < r && vowels_table.find(tolower(s[r])) == vowels_table.end()) --r;
+      // exchange these two vowels
+      swap(s[l], s[r]);
+      ++l;
+      --r;
+    }
+    return s;
+  }
+};
